77397f919b695b1e44a8c07c1ba81679c90a0ed0
Zia Khan <ziaukhan@hotmail.com>
2015-08-22 17:02:27 +0500

    completed step1

---
+++ step1_structure/readme.md
@@ -0,0 +1,2 @@
+Read:
+A flexiable structure that can be used with Angular 1.x, Aungular 2, and Firebase hosting as well.
---
+++ step1_structure/server/built/app.js
@@ -0,0 +1,13 @@
+/// <reference path='./typings/tsd.d.ts' />
+var express = require('express');
+var app = express();
+app.set('client', __dirname + '../../client');
+app.engine('html', require('ejs').renderFile);
+app.get('/', function (req, res) {
+    res.render('../../client/index.html');
+});
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+    var listeningPort = server.address().port;
+    console.log('The server is listening on port: ' + listeningPort);
+});
