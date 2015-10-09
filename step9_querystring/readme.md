ee55b7375492086a67b19cdd428ecd3ad221871c
Rehanuddin <s.rehanuddin@gmail.com>
2015-10-09 19:32:48 +0500

    Step Query String Added

---
+++ step9_querystring/readme.md
@@ -0,0 +1,16 @@
+# req.query:
+An object containing a property for each query string parameter in the route. If there is no query string, it is the empty object, {}.
+
+// GET /search?q=tobi+ferret
+req.query.q
+// => "tobi ferret"
+
+// GET /shoes?order=desc&shoe[color]=blue&shoe[type]=converse
+req.query.order
+// => "desc"
+
+req.query.shoe.color
+// => "blue"
+
+req.query.shoe.type
+// => "converse"
---
+++ step9_querystring/src/server.ts
@@ -0,0 +1,50 @@
+/// <reference path='../typings/tsd.d.ts' />
+
+import express = require('express');
+import path = require('path');
+
+var app : express.Express = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '/../views'));
+app.set('view engine', 'ejs'); 
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get("/q", function(req, res){
+	res.json(req.query);
+});
+
+var users = [
+	{id:1 , name: "Rehan", age: 24},
+	{id:2 , name: "A", age: 24},
+	{id:3 , name: "B", age: 24},
+	{id:4 , name: "C", age: 24},
+	{id:5 , name: "D", age: 24},
+	{id:6 , name: "E", age: 24},
+	{id:7 , name: "f", age: 24},
+	{id:8 , name: "G", age: 24},
+]
+app.get("/", function(req, res){
+	res.render("./index");
+});
+
+app.get("/search", function(req, res){
+	
+	console.log(req.query.q)
+
+	for(var i = 0; i < users.length; i++){
+		if((users[i].name).toLowerCase() == (req.query.q).toLowerCase()){
+			res.json(users[i]);
+			return;
+		}
+	}
+	res.send("User Not Found");
+});
+var port: number = process.env.PORT || 3000;
+var server = app.listen(port, () => {
+	var listeningPort: number = server.address().port;
+	console.log('The server is listening on port: ' + listeningPort);
+});
+
+
