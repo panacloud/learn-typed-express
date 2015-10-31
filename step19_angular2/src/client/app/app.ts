95f7ea2313dd08c312b256d419bf21f5ffc691a2
Rehanuddin <s.rehanuddin@gmail.com>
2015-11-01 03:15:36 +0500

    Removed Duplicates

--- step19_angular2/src/client/app/app.ts
+++
@@ -1,7 +0,0 @@
-import {Component, bootstrap} from 'angular2/angular2';
-@Component({
-    selector: 'my-app',
-    template: '<h1>Angular 2 App with Typed Node.js</h1>'
-})
-class AppComponent { }
-bootstrap(AppComponent);
