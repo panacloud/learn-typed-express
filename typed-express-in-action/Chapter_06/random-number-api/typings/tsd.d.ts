a6b0273635528363e7f9175fdb4713ef32c25e05
Zia Khan <ziaukhan@hotmail.com>
2015-11-14 11:46:47 +0500

    completed chapter 6

---
+++ typed-express-in-action/Chapter_06/random-number-api/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="node/node.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="morgan/morgan.d.ts" />
