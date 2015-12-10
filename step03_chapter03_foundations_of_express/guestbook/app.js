18581726c4f4bcbdbede5033dd88babea5ada3e7
Inzamam Malik <malikasinger1@gmail.com>
2015-12-10 15:23:05 +0500

    Update app.js

--- step03_chapter03_foundations_of_express/guestbook/app.js
+++ step03_chapter03_foundations_of_express/guestbook/app.js
@@ -1,2 +1,18 @@
 /// <reference path="./typings/tsd.d.ts" />
-console.log(require('url').parse('http://localhost:3000/1?p=2').pathname);
+var express = require("express");
+var url = require('url');
+var app = express();
+app.use(function (req, res, next) {
+    var parsedUrl = url.parse(req.url);
+    //few things whicyh is parser by "url.parse()" method
+    console.log(parsedUrl.path);
+    console.log(parsedUrl.pathname);
+    console.log(parsedUrl.query);
+    console.log(parsedUrl.search);
+    console.log(parsedUrl.href);
+    //and etc.
+    next();
+});
+app.listen(3000, function () {
+    console.log("listening at 3000 \ngoto postman and request like this \nhttp://localhost:3000/1?p=2");
+});
