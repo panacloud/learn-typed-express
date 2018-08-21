cc989695c4609d38b9f69292581fc5c392eada35
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-08-22 01:19:19 +0500

    2.4

--- step02_chapter02_the_basics_of_node.js/node_webserver/app.js
+++ step02_chapter02_the_basics_of_node.js/node_webserver/app.js
@@ -1,8 +1,11 @@
-/// <reference path="./typings/tsd.d.ts" />
-var http = require("http");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
 function requestHandler(request, response) {
     console.log("In comes a request to: " + request.url);
     response.end("Hello, world!");
 }
-var server = http.createServer(requestHandler);
-server.listen(3000);
+var server = http_1.createServer(requestHandler);
+server.listen(3000, function () {
+    console.log("listening on port 3000");
+});
